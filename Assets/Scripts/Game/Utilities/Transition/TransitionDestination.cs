f666c450890025b4589d66f09301cdb9a7ee3280
RCrobotcat <136684976+RCrobotcat@users.noreply.github.com>
2024-10-26 16:29:27 +0800

    添加了酒馆二楼场景

--- Assets/Scripts/Game/Utilities/Transition/TransitionDestination.cs
+++ Assets/Scripts/Game/Utilities/Transition/TransitionDestination.cs
@@ -4,7 +4,14 @@ public class TransitionDestination : MonoBehaviour
 {
     public enum DestinationTag
     {
-        BarEnter, BarExit, ChurchEnter, ChurchExit, DungeonEnter, DungeonExit
+        BarEnter,
+        BarExit,
+        ChurchEnter,
+        ChurchExit,
+        DungeonEnter,
+        DungeonExit,
+        SecondFloorEnter,
+        SecondFloorExit
     }
 
     public DestinationTag destinationTag;
